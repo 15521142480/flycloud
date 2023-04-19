da2518aa801d2bccbb1fbc8aec54478e1e58facb
laixueshi <2570078967@qq.com>
2023-04-19 18:30:40 +0800

    /

---
+++ flycloud-common/flycloud-common-doc/src/main/java/com/fly/common/doc/config/properties/SwaggerProperties.java
@@ -0,0 +1,69 @@
+package com.fly.common.doc.config.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * swagger 配置属性
+ *
+ * @author lxs
+ * @date 2023/4/19
+ */
+@Data
+@ConfigurationProperties(prefix = "swagger")
+public class SwaggerProperties {
+    /**
+     * 是否开启swagger
+     */
+    private Boolean enabled = true;
+
+    /**
+     * swagger会解析的包路径
+     **/
+    private String basePackage = "";
+
+    /**
+     * swagger会解析的url规则
+     **/
+    private List<String> basePath = new ArrayList<>();
+
+    /**
+     * 在basePath基础上需要排除的url规则
+     **/
+    private List<String> excludePath = new ArrayList<>();
+
+    /**
+     * 需要排除的服务
+     */
+    private List<String> ignoreProviders = new ArrayList<>();
+
+    /**
+     * 标题
+     **/
+    private String title = "";
+
+    /**
+     * 网关
+     */
+    private String gateway;
+
+    /**
+     * 获取token
+     */
+    private String tokenUrl;
+
+    /**
+     * 作用域
+     */
+    private String scope;
+
+    /**
+     * 服务转发配置
+     */
+    private Map<String, String> services;
+
+}
