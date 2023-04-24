7b491ee9f85d649f2abe1fb39becc93667faf972
laixueshi <2570078967@qq.com>
2023-04-24 20:00:48 +0800

    部署测试环境-test

---
+++ flycloud-mall/src/main/java/com/fly/mall/app/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.fly.mall.app.controller;
+
+import com.fly.common.model.R;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 'app端'首页-控制层
+ *
+ * @author lxs
+ * @date 2023/4/24
+ */
+@RestController
+@RequestMapping("/app/home")
+public class HomeController {
+
+
+    /**
+     * 获取推荐商品列表
+     */
+    @GetMapping("/getRecommendGoodsList")
+    public R<List<String>> getRecommendGoodsList() {
+
+        List<String> goodsList = new ArrayList<>();
+        goodsList.add("商品1");
+        goodsList.add("商品2");
+        goodsList.add("商品3");
+        return R.ok(goodsList);
+    }
+
+
+}
