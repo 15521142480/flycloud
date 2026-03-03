1f00ce2815636f2bcd958f8087a57ee347726d06
lxs <2570078967@qq.com>
2026-03-04 01:23:13 +0800

    修复流程结束时流程变量导致流程显示问题

--- flycloud-bpm/src/main/java/com/fly/bpm/oa/listener/BpmOaLeaveInstanceEndListener.java
+++ flycloud-bpm/src/main/java/com/fly/bpm/oa/listener/BpmOaLeaveInstanceEndListener.java
@@ -2,9 +2,11 @@ package com.fly.bpm.oa.listener;
 
 import com.fly.bpm.oa.service.IBpmOaLeaveService;
 import com.fly.common.constant.CommonConstants;
+import com.fly.common.constant.bpm.BpmnVariableConstants;
 import com.fly.common.enums.bpm.BpmProcessInstanceStatusEnum;
 import com.fly.common.exception.BpmException;
 import com.fly.common.utils.StringUtils;
+import org.flowable.engine.RuntimeService;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.flowable.engine.delegate.ExecutionListener;
 import org.springframework.stereotype.Component;
@@ -23,14 +25,24 @@ public class BpmOaLeaveInstanceEndListener implements ExecutionListener {
     @Resource
     private IBpmOaLeaveService leaveService;
 
+    @Resource
+    private RuntimeService runtimeService;
+
 
     @Override
     public void notify(DelegateExecution delegateExecution) {
 
         String businessKey = delegateExecution.getProcessInstanceBusinessKey();
-        if (StringUtils.isBlank(businessKey)) {
-            throw new BpmException(CommonConstants.FAIL, "流程businessKey为空！");
+        String instanceId = delegateExecution.getProcessInstanceId();
+        if (StringUtils.isBlank(businessKey) || StringUtils.isBlank(businessKey)) {
+            throw new BpmException(CommonConstants.FAIL, "businessKey || instanceId为空！");
         }
+
+        // todo 2026/03/04 如下，涉及流程重要数据需要修改多处，设计上可以优化下；如抽出一个自定义的流程实例表记录流程实例的最新信息，而不是放在流程变量里？
+        // 流程实例状态变量
+        runtimeService.setVariable(instanceId, BpmnVariableConstants.PROCESS_INSTANCE_VARIABLE_STATUS, BpmProcessInstanceStatusEnum.APPROVE.getStatus());
+
+        // 业务表流程状态
         leaveService.updateLeaveStatus(Long.valueOf(businessKey), BpmProcessInstanceStatusEnum.APPROVE.getStatus());
     }
 }
